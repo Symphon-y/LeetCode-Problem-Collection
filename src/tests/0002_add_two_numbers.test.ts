2c79c2520aa4d6c090bd606ea3c6e25c14b820ac
Travis Redden <travisredden@outlook.com>
2023-10-11 09:22:53 -0500

    formatting

--- src/tests/0002_add_two_numbers.test.ts
+++ src/tests/0002_add_two_numbers.test.ts
@@ -4,17 +4,16 @@ import { linkedListValuesToArray } from '../utilities/0002_add_two_numbers/Linke
 
 test('should output [7,0,8] when l1 input is [2, 4, 3], and l2 input is [5, 6, 4]', () => {
   const ListOne = new LinkedList();
-  const ListTwo = new LinkedList();
-
-  const testList = new LinkedList();
   ListOne.append(2);
   ListOne.append(4);
   ListOne.append(3);
 
+  const ListTwo = new LinkedList();
   ListTwo.append(5);
   ListTwo.append(6);
   ListTwo.append(4);
 
+  const testList = new LinkedList();
   testList.append(7);
   testList.append(0);
   testList.append(8);
