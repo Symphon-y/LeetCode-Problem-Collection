b3db99cd697e5cda7cab18b75d0e79c0b3b3acfc
Travis Redden <travisredden@outlook.com>
2023-10-15 14:16:49 -0500

    0007 solution

--- src/leetcode/0007_reverse_integer/solution.ts
+++ src/leetcode/0007_reverse_integer/solution.ts
@@ -1,3 +1,15 @@
+export const isNumberOutOfRange = (num: number): boolean => {
+  return num > 2147483647 || num < -2147483648;
+};
+
 export const reverse = (x: number): number => {
-  return 0;
+  let number: number = Math.abs(x); // holds a new copy of the absolute value of the input integer
+  let result: number = 0;
+  while (number > 0) {
+    const lastDigit = number % 10;
+    result = result * 10 + lastDigit;
+    number = Math.floor(number / 10);
+  }
+  const sign = x < 0 ? -1 : 1; // creates a variable either 1 or -1 depending on input numbers quality
+  return isNumberOutOfRange(result * sign) ? 0 : result * sign;
 };
