26085af8b47bb8d64da15d2c82eaf5e152b33e48
Travis Redden <travisredden@outlook.com>
2023-10-15 12:15:12 -0500

    0006 | WIP solution

--- src/leetcode/0006_zigzag_conversion/solution.ts
+++ src/leetcode/0006_zigzag_conversion/solution.ts
@@ -1,23 +1,62 @@
-export const convert = (s: string, numRows: number): string => {
-  let result: string = '';
+const createPattern = (s: string, numRows: number): string => {
   let isIndexAscending: boolean = true;
   let i: number = 0;
 
-  while (result.length < s.length) {
+  let pattern: string = '';
+  for (let j = 0; j < s.length; j++) {
     if (isIndexAscending) {
-      result += s[i];
+      pattern += i;
       i++;
-      if (i === numRows - 1) {
-        isIndexAscending = !isIndexAscending;
+      if (i >= numRows - 1) {
+        isIndexAscending = false;
       }
     } else {
-      result += s[i];
-      i++;
-      if (i === numRows - 1) {
-        isIndexAscending = !isIndexAscending;
+      pattern += i;
+      i--;
+      if (i <= 0) {
+        isIndexAscending = true;
       }
     }
   }
+  return pattern;
+};
+
+const createZigZagSchema = (
+  s: string,
+  pattern: string
+): { [key: string]: any } => {
+  let objectCache: { [key: string]: any } = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (!objectCache[pattern[i]]) {
+      objectCache[pattern[i]] = [];
+    }
+    objectCache[pattern[i]].push(s[i]);
+  }
+
+  return objectCache;
+};
+
+export const convert = (s: string, numRows: number): string => {
+  // Handle edge cases
+  if (numRows === 1) {
+    return s;
+  } else if (numRows === 0) {
+    return '';
+  }
+
+  let result: string = '';
+  const pattern = createPattern(s, numRows);
+  const schema = createZigZagSchema(s, pattern);
+  const arraysInOrder = Object.keys(schema).sort(
+    (a: string, b: string): number => {
+      return Number(a) - Number(b);
+    }
+  );
+  arraysInOrder.forEach((key) => {
+    const convertedString = schema[key].join('');
+    result += convertedString;
+  });
 
   return result;
 };
--- src/tests/0006_zigzag_conversion/0006_zigzag_conversion.test.ts
+++ src/tests/0006_zigzag_conversion/0006_zigzag_conversion.test.ts
@@ -3,9 +3,15 @@ import { convert } from '../../leetcode/0006_zigzag_conversion/solution.ts';
 test('If the input string is PAYPALISHIRING with 3 rows the answer should be PAHNAPLSIIGYIR', () => {
   return expect(convert('PAYPALISHIRING', 3)).toEqual('PAHNAPLSIIGYIR');
 });
+
 test('If the input string is PAYPALISHIRING with 4 rows the answer should be PINALSIGYAHRPI', () => {
   return expect(convert('PAYPALISHIRING', 4)).toEqual('PINALSIGYAHRPI');
 });
+
 test('If the input string is A with 1 rows the answer should be A', () => {
   return expect(convert('A', 1)).toEqual('A');
+});
+
+test('If the input string is PAYPALISHIRING with 11 rows the answer should be PAYPALISGHNIIR', () => {
+  return expect(convert('PAYPALISHIRING', 11)).toEqual('PAYPALISGHNIIR');
 });
