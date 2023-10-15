fa5283867e4fbcffddbfc949178251e926d85267
Travis Redden <travisredden@outlook.com>
2023-10-15 10:02:13 -0500

    0006 WIP

--- src/leetcode/0006_zigzag_conversion/solution.ts
+++ src/leetcode/0006_zigzag_conversion/solution.ts
@@ -1,4 +1,23 @@
 export const convert = (s: string, numRows: number): string => {
   let result: string = '';
+  let isIndexAscending: boolean = true;
+  let i: number = 0;
+
+  while (result.length < s.length) {
+    if (isIndexAscending) {
+      result += s[i];
+      i++;
+      if (i === numRows - 1) {
+        isIndexAscending = !isIndexAscending;
+      }
+    } else {
+      result += s[i];
+      i++;
+      if (i === numRows - 1) {
+        isIndexAscending = !isIndexAscending;
+      }
+    }
+  }
+
   return result;
 };
